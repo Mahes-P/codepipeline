c64542cc1f3253db75055a46fb9feff361a8bfe1
Maheswar <maheswarkunu@gmail.com>
2020-09-23 00:38:26 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-rm -rf /home/centos/appspec.yml /home/centos/start_server.sh /home/centos/stop_server.sh /home/centos/helloworld-0.0.1-SNAPSHOT.jar
-
-PID=`ps -ef | grep 'helloworld-0.0.1-SNAPSHOT.jar' | grep -v grep | awk '{print $2}'`
-if [ -n $PID ];then
-   kill -9 $PID
-fi
+echo hello
