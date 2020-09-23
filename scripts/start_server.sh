c1039ac4ec7834649ae3b9291773879de008910d
Maheswar <maheswarkunu@gmail.com>
2020-09-23 10:22:53 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-nohup java -jar /home/centos/helloworld-0.0.1-SNAPSHOT.jar 2>/dev/null  &
+#nohup java -jar /home/centos/helloworld-0.0.1-SNAPSHOT.jar 2>/dev/null  &
+java -jar /home/centos/helloworld-0.0.1-SNAPSHOT.jar 
 PID=`ps -ef | grep 'helloworld-0.0.1-SNAPSHOT.jar' | grep -v grep | awk '{print $2}'`
 if [ $PID ];then
    exit 0
